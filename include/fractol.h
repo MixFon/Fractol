611bde8e9205a6e4b8144e781f0d7e3cc8af7db2
Wispmother Draugr scourge <widraugr@su-f6.21-school.ru>
2019-11-18 18:00:11 +0300

    Fractol

---
+++ include/fractol.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fractol.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: widraugr <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/11/18 14:40:09 by widraugr          #+#    #+#             */
+/*   Updated: 2019/11/18 17:37:47 by widraugr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FRACTOL_C
+# define FRACTOL_C
+
+# include "../libft/libft.h"
+# include "mlx.h"
+
+# define WIDTH	1000
+# define HIEGHT	500
+# define TITLE	"Fractol"
+
+typedef struct	s_frac
+{
+		void	*mlx;
+		void	*window;
+}				t_frac;
+
+#endif
---
+++ source/main.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: widraugr <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/11/18 14:34:02 by widraugr          #+#    #+#             */
+/*   Updated: 2019/11/18 17:38:28 by widraugr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/fractol.h"
+
+void	init(t_frac *frac)
+{
+	frac->mlx = mlx_init();
+	frac->window = mlx_new_window(frac->mlx, WIDTH, HIEGHT, TITLE);
+
+}
+
+int main(int ac, char **av)
+{
+	t_frac frac;
+
+	init(&frac);
+	mlx_loop(frac.mlx);
+	return (0);
+}
