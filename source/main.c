569b7528cbb947c5d75ea9047796e2b9932bb8a3
Wispmother Draugr scourge <widraugr@su-e6.21-school.ru>
2019-11-20 16:20:17 +0300

    Fractol

--- source/main.c
+++ source/main.c
@@ -6,7 +6,7 @@
 /*   By: widraugr <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/18 14:34:02 by widraugr          #+#    #+#             */
-/*   Updated: 2019/11/18 19:33:35 by widraugr         ###   ########.fr       */
+/*   Updated: 2019/11/20 14:38:41 by widraugr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,9 @@ void	init(t_frac *frac)
 {
 	frac->mlx = mlx_init();
 	frac->window = mlx_new_window(frac->mlx, WIDTH, HIEGHT, TITLE);
-
 }
 
-int	press_key(int key, int i)
+int	press_key(int key)
 {
 	ft_printf("key = [%d]\n", key);
 	if (key == K_ESC)
@@ -32,7 +31,7 @@ int main(int ac, char **av)
 	t_frac frac;
 
 	init(&frac);
-	mlx_key_hook(&frac.window, press_key, 0);
+	mlx_key_hook(frac.window, press_key, 0);
 	mlx_loop(frac.mlx);
 	return (0);
 }
