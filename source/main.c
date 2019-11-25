34912e353061d1ac9bd29184a0a6bc8afef58da3
MixFon <mixfon@rambler.ru>
2019-11-25 21:05:08 +0300

    Fractol

--- source/main.c
+++ source/main.c
@@ -18,6 +18,8 @@ void	init(t_frac *frac)
 	frac->max_size_adr = WIDTH * 4 * HEIGHT;
 	frac->bits_adr = 4;
 	frac->endian = 0;
+	frac->comp.re = 0;
+	frac->comp.im = 0;
 	frac->mlx = mlx_init();
 	frac->window = mlx_new_window(frac->mlx, WIDTH, HEIGHT, TITLE);
 	frac->img = mlx_new_image(frac->mlx, WIDTH, HEIGHT);
@@ -75,13 +77,74 @@ void	drow_line(t_frac *frac)
 		drow_pixel_to_adr(frac, 0 + WIDTH / 2, i + HEIGHT / 2, 0xff0000);
 }
 
+t_compl	get_compl(double re, double im)
+{
+	t_compl comp;
+	
+	comp.re = re;
+	comp.im = im;
+	return (comp);
+}
+
+void	drow_frac(t_frac *frac)
+{
+	int		x;
+	int		y;
+	int		iter;
+	int		max_iter;
+	int		color;
+	t_compl comp;
+	t_compl min;
+	t_compl max;
+	t_compl factor;
+	t_compl c;
+	double	t;
+
+	min = get_compl(-2.0, -2.0);
+	max.re = 2.0;
+	max.im = min.im + (max.re - min.re) * HEIGHT / WIDTH;
+
+	factor = get_compl(
+    (max.re - min.re) / (WIDTH - 1),
+    (max.im - min.im) / (HEIGHT - 1));
+
+	y = -1;
+	max_iter = 10;
+	while(++y < HEIGHT)
+	{
+		c.im = max.im - y * factor.im;
+		x = -1;
+		while(++x < WIDTH)
+		{
+			c.re = min.re + x * factor.re;
+			comp = get_compl(c.re, c.im);
+			iter = -1;
+			while(pow(comp.re, 2.0) + pow(comp.im, 2.0) <= 4
+					&& ++iter < max_iter)
+			{
+				comp = get_compl(
+						pow(comp.re, 2.0) - pow(comp.im, 2.0) + c.re,
+						2.0 * comp.re * comp.im + c.im);
+			}
+			ft_printf("%d ", iter);
+			if (iter == -1)
+				mlx_pixel_put(frac->mlx,frac->window, x, y, 0xff); 
+			if (iter == 0)
+				mlx_pixel_put(frac->mlx,frac->window, x, y, 0xff00); 
+			if (iter == 50)
+				mlx_pixel_put(frac->mlx,frac->window, x, y, 0xffff); 
+		}
+	}
+}
+
 int main(int ac, char **av)
 {
 	t_frac frac;
 
 	init(&frac);
 	drow_line(&frac);
-	put_img(&frac);
+	drow_frac(&frac);
+	//put_img(&frac);
 	mlx_key_hook(frac.window, press_key, 0);
 	mlx_mouse_hook(frac.window, press_mouse_key, 0);
 	//mlx_loop_hook(frac.mlx,  loop_hook, 0);
