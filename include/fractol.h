1b6e4691747ee36cc86314bd3356d46fcb6ff9fd
Wispmother Draugr scourge <widraugr@su-f6.21-school.ru>
2019-12-05 19:28:42 +0300

    Fractol

--- include/fractol.h
+++ include/fractol.h
@@ -6,7 +6,7 @@
 /*   By: widraugr <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/18 14:40:09 by widraugr          #+#    #+#             */
-/*   Updated: 2019/11/25 12:25:50 by widraugr         ###   ########.fr       */
+/*   Updated: 2019/12/05 19:28:34 by widraugr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,17 +18,20 @@
 # include <math.h>
 # include <pthread.h>
 
-# define WIDTH	1000
-# define HEIGHT	600
+# define WIDTH	1200
+# define HEIGHT	800
 # define TITLE	"Fractol"
 
-# define COUNT_PTH	50
+# define COUNT_PTH	100
+
+# define IMGPATH	"./image/Fractol.xpm"
 
 # define K_ESC		53
 # define K_UP		126
 # define K_DOWN		125
 # define K_LEFT		123
 # define K_RIGHT	124
+# define K_ENTER	36
 
 # define W_UP	4
 # define W_DOWN	5
@@ -42,6 +45,27 @@
 # define K_C	8
 # define K_V	9
 # define K_R	15
+# define K_M	46
+# define K_N	45
+# define K_B	11
+
+# define K_H	4
+# define K_J	38
+# define K_K	40
+# define K_L	37
+
+# define K_1	18
+# define K_2	19
+# define K_3	20
+# define K_4	21
+# define K_5	23
+# define K_6	22
+# define K_7	26
+# define K_8	28
+
+# define K_N2	84
+# define K_N3	85
+# define K_N4	86
 
 typedef struct	s_compl
 {
@@ -56,13 +80,25 @@ typedef struct	s_parm
 	int			y_end;
 	int			iter;
 }				t_parm;
+
+typedef struct	s_flags
+{
+	int			julia : 1;
+	int			mandelbrot : 1;
+	int			ship : 1;
+	int			z_2 : 1;
+	int			z_3 : 1;
+	int			z_4 : 1;
+	int			greeting : 1;
+}				t_flags;
+
 typedef struct	s_frac
 {
 		void	*mlx;
 		void	*window;
 		void	*img;
+		void	*greeting;
 		char	*data_adr;
-		char	*data_adr_bazil;
 		size_t	max_size_adr;
 		int		size_adr;
 		int		bits_adr;
@@ -72,17 +108,15 @@ typedef struct	s_frac
 		t_compl	min;
 		t_compl	max;
 		t_compl	k;
-		int		julia;
-		int		mandelbrot;
-		double	color;;
+		double	color;
 		pthread_t st_pth[COUNT_PTH];
-
-		void	*img_bazil;
-		int		size_adr_basil;
+		t_flags	flags;
 }				t_frac;
 
 void	*drow_frac(void *arg);//t_frac *frac);
 void	create_theads(t_frac *frac);
 t_compl	get_compl(double re, double im);
+void	drow_pixel_to_adr(t_frac *frac, int x, int y, int color);
+void	greeting(t_frac *frac);
 
 #endif
--- source/main.c
+++ source/main.c
@@ -6,7 +6,7 @@
 /*   By: widraugr <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/18 14:34:02 by widraugr          #+#    #+#             */
-/*   Updated: 2019/12/04 16:58:51 by widraugr         ###   ########.fr       */
+/*   Updated: 2019/12/05 19:28:33 by widraugr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,24 +14,28 @@
 
 void	init(t_frac *frac)
 {
+	int	w;
+	int	h;
+
 	frac->size_adr = WIDTH;
 	frac->max_size_adr = WIDTH * 4 * HEIGHT;
 	frac->bits_adr = 4;
 	frac->endian = 0;
-	frac->max_iter = 10;
-	frac->min.re = -2.0;
-	frac->min.im = -1.25;
-	frac->max.re = 2.0;
-	frac->zoom = 0.1;
+	frac->max_iter = 30;
+	frac->min.re = -2.15;
+	frac->min.im = -1.45;
+	frac->max.re = 2.15;
+	frac->zoom = 0.05;
 	frac->color = 1;
 	frac->max.im = frac->min.im + (frac->max.re - frac->min.re)
 		* HEIGHT / WIDTH;
 	frac->mlx = mlx_init();
 	frac->window = mlx_new_window(frac->mlx, WIDTH, HEIGHT, TITLE);
-
 	frac->img = mlx_new_image(frac->mlx, WIDTH, HEIGHT);
 	frac->data_adr = mlx_get_data_addr(frac->img, &frac->bits_adr,
 			&frac->size_adr, &frac->endian);
+	if (!(frac->greeting = mlx_xpm_file_to_image(frac->mlx, IMGPATH, &w, &h)))
+		sys_err("Error file greeting.\n");
 }
 
 int	press_mouse_key(int key, int x, int y, t_frac *frac)
@@ -54,11 +58,11 @@ int	press_mouse_key(int key, int x, int y, t_frac *frac)
 		frac->max.im = frac->min.im + (frac->max.re - frac->min.re)
 			* HEIGHT / WIDTH;
 	}
+	ft_printf("re = [%0f], im = [%0f], minim = [%0f]\n",
+			frac->min.re, frac->max.re, frac->min.im);
 	create_theads(frac);
-	mlx_put_image_to_window(frac->mlx, frac->window, frac->img, 0, 0);
 	return (0);
 }
-
 
 int	press_key(int key, t_frac *frac)
 {
@@ -66,39 +70,13 @@ int	press_key(int key, t_frac *frac)
 	if (key == K_ESC)
 		sys_err("Normal exit\n");
 	else if (key == K_DOWN)
-	{
-		frac->k.im -= 0.05;
-		/*
-		frac->min.im -= frac->zoom;
-		frac->max.im = frac->min.im + (frac->max.re - frac->min.re)
-			* HEIGHT / WIDTH;
-		*/
-	}
+		frac->k.im -= 0.01;
 	else if (key == K_UP)
-	{
-		frac->k.im += 0.05;
-		/*
-		frac->min.im += frac->zoom;
-		frac->max.im = frac->min.im + (frac->max.re - frac->min.re)
-			* HEIGHT / WIDTH;
-		*/
-	}
+		frac->k.im += 0.01;
 	else if (key == K_LEFT)
-	{
-		frac->k.re += 0.05;
-		/*
-		frac->min.re -= frac->zoom;
-		frac->max.re -= frac->zoom;
-		*/
-	}
+		frac->k.re += 0.01;
 	else if (key == K_RIGHT)
-	{
-		frac->k.re -= 0.05;
-		/*
-		frac->min.re += frac->zoom;
-		frac->max.re += frac->zoom;
-		*/
-	}
+		frac->k.re -= 0.01;
 	else if (key == K_C)
 	{
 		frac->min.re -= frac->zoom;
@@ -115,6 +93,20 @@ int	press_key(int key, t_frac *frac)
 		frac->max.im = frac->min.im + (frac->max.re - frac->min.re)
 			* HEIGHT / WIDTH;
 	}
+	else if (key == K_N)
+		frac->flags.julia = !frac->flags.julia;
+	else if (key == K_M)
+	{
+		frac->flags.ship = 0;
+		frac->flags.julia = 0;
+		frac->flags.mandelbrot = 1;
+	}
+	else if (key == K_B)
+	{
+		frac->flags.ship = 1;
+		frac->flags.julia = 0;
+		frac->flags.mandelbrot = 0;
+	}
 	else if (key == K_R)
 	{
 		frac->min.re = -2.0;
@@ -123,22 +115,31 @@ int	press_key(int key, t_frac *frac)
 		frac->max.im = frac->min.im + (frac->max.re - frac->min.re)
 			* HEIGHT / WIDTH;
 	}
+	else if (key == K_1)
+		frac->k = get_compl(0, 0);
+	else if (key == K_2)
+		frac->k = get_compl(-0.2, 0);
+	else if (key == K_3)
+		frac->k = get_compl(-0.5, 0);
+	else if (key == K_4)
+		frac->k = get_compl(-0.75, 0);
+	else if (key == K_5)
+		frac->k = get_compl(-1, 0);
+	else if (key == K_6)
+		frac->k = get_compl(-0.12, 0.74);
+	else if (key == K_7)
+		frac->k = get_compl(-1.77, 0);
+	else if (key == K_8)
+		frac->k = get_compl(0, 1);
 	else if (key == K_Q)
 	{
-		frac->k = get_compl(0.1, 1);
-		ft_putendl("hook");
-		ft_printf("re = [%f], im = {%f}\n", frac->k.re, frac->k.im);
-		//ft_printf("x = [%d], y = {%d}\n", x, y);
-		create_theads(frac);
+		if (frac->zoom - 0.01 <= 0)
+			frac->zoom -= 0.001;
+		else
+			frac->zoom -= 0.01;
 	}
 	else if (key == K_W)
-	{
-		frac->k = get_compl(0.56, 0.11);
-		ft_putendl("hook");
-		ft_printf("re = [%f], im = {%f}\n", frac->k.re, frac->k.im);
-		//ft_printf("x = [%d], y = {%d}\n", x, y);
-		create_theads(frac);
-	}
+		frac->zoom += 0.01;
 	else if (key == K_A)
 		frac->color += 0.1;
 	else if (key == K_S)
@@ -147,27 +148,51 @@ int	press_key(int key, t_frac *frac)
 		frac->max_iter += 10;
 	else if (key == K_X)
 		frac->max_iter -= 10;
-	else if (key == K_2)
+	else if (key == K_H)
+	{
+		frac->min.re += 0.05;
+		frac->max.re += 0.05;
+	}
+	else if (key == K_L)
+	{
+		frac->min.re -= 0.05;
+		frac->max.re -= 0.05;
+	}
+	else if (key == K_J)
+	{
+		frac->min.im += 0.05;
+		frac->max.im += 0.05;
+	}
+	else if (key == K_K)
+	{
+		frac->min.im -= 0.05;
+		frac->max.im -= 0.05;
+	}
+	else if (key == K_X)
+		frac->max_iter -= 10;
+	else if (key == K_X)
+		frac->max_iter -= 10;
+	else if (key == K_N2)
 	{
 		frac->flags.z_2 = 1;
 		frac->flags.z_3 = 0;
 		frac->flags.z_4 = 0;
 	}
-	else if (key == K_3)
+	else if (key == K_ENTER)
+		frac->flags.greeting = !frac->flags.greeting;
+	else if (key == K_N3)
 	{
 		frac->flags.z_2 = 0;
 		frac->flags.z_3 = 1;
 		frac->flags.z_4 = 0;
 	}
-	else if (key == K_4)
+	else if (key == K_N4)
 	{
 		frac->flags.z_2 = 0;
 		frac->flags.z_3 = 0;
 		frac->flags.z_4 = 1;
 	}
-
 	create_theads(frac);
-	//mlx_put_image_to_window(frac->mlx, frac->window, frac->img, 0, 0);
 	return (0);
 }
 
@@ -210,14 +235,14 @@ int nephroida(t_compl *c)
 	double	roc;
 	double	p;
 	
-	a = 1.0;
-	p = 2/3;
+	a = 0.38;
+	p = (double)2/3;
 	ro = pow(pow((c->re), 2.0) + pow(c->im, 2.0), 0.5);
 	teta = atan2(c->im, c->re);
-	roc = pow(a, p) * (pow(cos(teta), p) + pow(sin(teta), p));
-	ft_printf("r = {%f}\n", roc);
-	ft_printf("r = {%f}\n", ro);
-	if (ro < roc / 2)
+	if (c->im < 0)
+		teta = (-1) * teta;
+	roc = pow(pow(a, p) * (pow(cos(teta / 2), p) + pow(sin(teta / 2), p)), 1.5);
+	if (ro < roc)
 		return (1);
 	return (0);
 }
@@ -262,12 +287,9 @@ void	create_color(t_frac *frac, int x, int y, int iter)
 	double	t;
 	int		color;
 
-	/*
 	if (iter == -1)
 		iter = 0;
-		*/
 	t = (double)iter / (double)frac->max_iter;
-
 	color = (int)(9 * (frac->color - t) * pow(t, 3) * 255);
 	color = color << 8;
 	color =color | (int)(15 * pow((frac->color - t), 2) * pow(t, 2) * 255);
@@ -276,165 +298,21 @@ void	create_color(t_frac *frac, int x, int y, int iter)
 	drow_pixel_to_adr(frac, x, y, color); 
 }
 
-void	get_iteration_z4(t_compl *c, int *max_iter, int *iter)
-{
-	t_compl comp;
-
-	comp = get_compl(c->re, c->im);
-	*iter = -1;
-	while(pow(comp.re, 2.0) + pow(comp.im, 2.0) <= 4
-			&& ++(*iter) < *max_iter)
-	{
-		comp = get_compl(
-			pow(comp.re, 4.0) - 6 * pow(comp.re, 2.0) * pow(comp.im, 2.0) +
-			pow(comp.im, 4.0) + c->re,
-			4 * pow(comp.re, 3.0) * comp.im - 4 * comp.re * pow(comp.im, 3.0) +
-			c->im);
-	}
-}
-
-void	get_iteration_z3(t_compl *c, int *max_iter, int *iter)
-{
-	t_compl comp;
-
-	comp = get_compl(c->re, c->im);
-	*iter = -1;
-	while(pow(comp.re, 2.0) + pow(comp.im, 2.0) <= 4
-			&& ++(*iter) < *max_iter)
-	{
-		comp = get_compl(
-				pow(comp.re, 3.0) - 3 * comp.re * pow(comp.im, 2.0) + c->re,
-				3 * pow(comp.re, 2.0) * comp.im - pow(comp.im, 3.0) + c->im);
-	}
-}
-
-void	get_iteration_cent_julia(t_compl *c, t_compl *k, int *max_iter, int *iter)
-{
-	t_compl comp;
-
-	comp = get_compl(c->re, c->im);
-	*iter = -1;
-	while(pow(comp.re, 2.0) + pow(comp.im, 2.0) <= 4
-			&& ++(*iter) < *max_iter)
-	{
-		comp = get_compl(
-				pow(comp.re, 3.0) - 3 * comp.re * pow(comp.im, 2.0) + k->re,
-				3 * pow(comp.re, 2.0) * comp.im - pow(comp.im, 3.0) + k->im);
-	}
-
-}
-
-void	get_iteration_z4_julia(t_compl *c, t_compl *k, int *max_iter, int *iter)
-{
-	t_compl comp;
-
-	comp = get_compl(c->re, c->im);
-	*iter = -1;
-	while(pow(comp.re, 2.0) + pow(comp.im, 2.0) <= 4
-			&& ++(*iter) < *max_iter)
-	{
-		comp = get_compl(
-			pow(comp.re, 4.0) - 6 * pow(comp.re, 2.0) * pow(comp.im, 2.0) +
-			pow(comp.im, 4.0) + k->re,
-			4 * pow(comp.re, 3.0) * comp.im - 4 * comp.re * pow(comp.im, 3.0) +
-			k->im);
-	}
-
-}
-
-void	get_iteration(t_compl *c, int *max_iter, int *iter)
-{
-	t_compl comp;
-
-	comp = get_compl(c->re, c->im);
-	*iter = -1;
-	if (!cardioda(&comp))
-	{
-		while(pow(comp.re, 2.0) + pow(comp.im, 2.0) <= 4
-				&& ++(*iter) < *max_iter)
-		{
-			comp = get_compl(
-					pow(comp.re, 2.0) - pow(comp.im, 2.0) + c->re,
-					2.0 * comp.re * comp.im + c->im);
-		}
-	}
-}
-
-void	get_iteration_ship_z3(t_compl *c, int *max_iter, int *iter)
-{
-	t_compl comp;
-
-	comp = get_compl(0, 0);
-	*iter = -1;
-	while(pow(comp.re, 2.0) + pow(comp.im, 2.0) <= 4
-			&& ++(*iter) < *max_iter)
-	{
-		comp = get_compl(
-				fabs(pow(comp.re, 3.0)) - 3 * fabs(comp.re) * pow(comp.im, 2.0) + c->re,
-				3.0 * pow(comp.re, 2.0) * fabs(comp.im)  + c->im);
-	}
-}
-
-void	get_iteration_ship(t_compl *c, int *max_iter, int *iter)
-{
-	t_compl comp;
-
-	comp = get_compl(c->re, c->im);
-	//comp = get_compl(0, 0);
-	*iter = -1;
-	while(pow(comp.re, 2.0) + pow(comp.im, 2.0) <= 4
-			&& ++(*iter) < *max_iter)
-	{
-		comp = get_compl(
-				pow(comp.re, 2.0) - pow(comp.im, 2.0) + c->re,
-				2.0 * fabs(comp.re * comp.im) + c->im);
-	}
-}
-
-void	get_iteration_julia_ship(t_compl *c, t_compl *k, int *max_iter, int *iter)
-{
-	t_compl comp;
-
-	comp = get_compl(c->re, c->im);
-	*iter = -1;
-	while(pow(comp.re, 2.0) + pow(comp.im, 2.0) <= 4
-			&& ++(*iter) < *max_iter)
-	{
-		comp = get_compl(
-				pow(comp.re, 2.0) - pow(comp.im, 2.0) + k->re,
-				2.0 * fabs(comp.re * comp.im) + k->im);
-	}
-
-}
-
-void	get_iteration_julia(t_compl *c, t_compl *k, int *max_iter, int *iter)
-{
-	t_compl comp;
-
-	comp = get_compl(c->re, c->im);
-	*iter = -1;
-	while(pow(comp.re, 2.0) + pow(comp.im, 2.0) <= 4
-			&& ++(*iter) < *max_iter)
-	{
-		comp = get_compl(
-				pow(comp.re, 2.0) - pow(comp.im, 2.0) + k->re,
-				2.0 * comp.re * comp.im + k->im);
-	}
-}
-
 int		check_fractol(t_flags *flags, t_compl *c)
 {
-	if (flags->mandelbrot)
+	if (!flags->julia)
+	{
 		if (flags->z_2)
 			if (cardioda(c))
 				return (1);
 		if (flags->z_3)
 			if (nephroida(c))
 				return (1);
+	}
 	return (0);
 }
 
-void	chose_fract(t_frac *frac, t_compl *c, int *iter)
+void	mandelbrot(t_frac *frac, t_compl *c, int *iter)
 {
 	t_compl comp;
 
@@ -448,11 +326,44 @@ void	chose_fract(t_frac *frac, t_compl *c, int *iter)
 			&& ++(*iter) < frac->max_iter)
 	{
 		if (frac->flags.z_2)
-			comp = get_compl(Z_2);
+			comp = get_compl(pow(comp.re, 2.0) - pow(comp.im, 2.0) + c->re,
+					2.0 * comp.re * comp.im + c->im);
 		else if (frac->flags.z_3)
-			comp = get_compl(Z_3);
+			comp = get_compl(pow(comp.re, 3.0) - 3 * comp.re * pow(comp.im, 2.0)
+		+ c->re, 3 * pow(comp.re, 2.0) * comp.im - pow(comp.im, 3.0) + c->im);
 		else if (frac->flags.z_4)
-			comp = get_compl(Z_4);
+			comp = get_compl(pow(comp.re, 4.0) - 6 * pow(comp.re, 2.0) *
+					pow(comp.im, 2.0) + pow(comp.im, 4.0) + c->re,
+					4 * pow(comp.re, 3.0) * comp.im - 4 * comp.re *
+					pow(comp.im, 3.0) + c->im);
+	}
+}
+
+void	ship(t_frac *frac, t_compl *c, int *iter)
+{
+	t_compl comp;
+
+	*iter = -1;
+	comp = get_compl(c->re, c->im);
+	if (frac->flags.julia)
+		c = &frac->k;
+	while (pow(comp.re, 2.0) + pow(comp.im, 2.0) <= 4
+			&& ++(*iter) < frac->max_iter)
+	{
+		if (frac->flags.z_2)
+			comp = get_compl(
+				pow(comp.re, 2.0) - pow(comp.im, 2.0) + c->re,
+				2.0 * fabsl(comp.re * comp.im) + c->im);
+		else if (frac->flags.z_3)
+			comp = get_compl(
+				fabs(pow(comp.re, 3.0)) - 3 * fabsl(comp.re) * pow(comp.im, 2.0) + c->re,
+				3.0 * pow(comp.re, 2.0) * fabsl(comp.im) - fabs(pow(comp.im, 3.0)) + c->im);
+		else if (frac->flags.z_4)
+			comp = get_compl(
+					pow(comp.re, 4.0) - 6 * pow(comp.re * comp.im, 2.0) + pow(comp.im, 4.0)
+					+ c->re,
+			4 * fabs(pow(comp.re, 3.0) * comp.im) - 4 * fabs(comp.re * pow(comp.im, 3.0))
+			+ c->im);
 	}
 }
 
@@ -467,9 +378,7 @@ void	*working_frac(void *arg)
 	factor = get_compl(
     (frac->max.re - frac->min.re) / (WIDTH - 1),
     (frac->max.im - frac->min.im) / (HEIGHT - 1));
-	//ft_printf("thid = [%d]\n", (int)(pthread_self()));
 	get_rage(frac, &par.y, &par.y_end);
-	//ft_printf("start = [%d] end = {%d}\n", par.y, par.y_end);
 	while(++par.y < par.y_end)
 	{
 		c.im = frac->max.im - par.y * factor.im;
@@ -477,7 +386,10 @@ void	*working_frac(void *arg)
 		while(++par.x < WIDTH)
 		{
 			c.re = frac->min.re + par.x * factor.re;
-			chose_fract(frac, &c, &par.iter);
+			if (frac->flags.mandelbrot)
+				mandelbrot(frac, &c, &par.iter);
+			else if (frac->flags.ship)
+				ship(frac, &c, &par.iter);
 			create_color(frac, par.x, par.y, par.iter); 
 		}
 	}
@@ -489,6 +401,11 @@ void	create_theads(t_frac *frac)
 	int i;
 
 	i = -1;
+	if (frac->flags.greeting)
+	{
+		mlx_put_image_to_window(frac->mlx, frac->window, frac->greeting, 0, 0);
+		return ;
+	}
 	while (++i < COUNT_PTH)
 	{
 		if ((pthread_create(&(frac->st_pth[i]), NULL, working_frac, frac)))
@@ -499,13 +416,6 @@ void	create_theads(t_frac *frac)
 		if (pthread_join(frac->st_pth[i], NULL))
 			sys_err("Error join thread\n");
 	mlx_put_image_to_window(frac->mlx, frac->window, frac->img, 0, 0);
-}
-
-int		expose_hook(int x, int y, t_frac *frac)
-{
-	ft_putendl("Hook");
-	ft_printf("!x = [%d], y = {%d}\n", x, y);
-	return (0);
 }
 
 int		loop_hook(int x, int y, t_frac *frac)
@@ -534,7 +444,8 @@ void	print_error_arg(void)
 	err = "Wrong fractal name.\n\n\
 	Use:\n\
 	./fractol Julia\n\
-	./fractol Mandelbrot\n";
+	./fractol Mandelbrot\n\
+	./fractol Ship\n";
 	sys_err(err);
 }
 
@@ -542,14 +453,19 @@ void	name_fractal(t_frac *frac, char **av)
 {
 	frac->flags.julia = 0;
 	frac->flags.mandelbrot = 0;
+	frac->flags.ship = 0;
 	frac->flags.z_2 = 1;
 	frac->flags.z_3 = 0;
 	frac->flags.z_4 = 0;
+	frac->flags.greeting = 1;
 	if (!(ft_strcmp(av[1], "Julia")))
 		frac->flags.julia = 1;
 	else if (!(ft_strcmp(av[1], "Mandelbrot")))
 		frac->flags.mandelbrot = 1;
-	ft_printf("J = {%d} M = [%d]\n", frac->flags.julia, frac->flags.mandelbrot);
+	else if (!(ft_strcmp(av[1], "Ship")))
+		frac->flags.ship = 1;
+	else
+		print_error_arg();
 }
 
 int		main(int ac, char **av)
